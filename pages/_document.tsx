01753f7470902c279ba7a76920c449d8ae6c6be3
okamoto-tosei <tosei393939@gmail.com>
2023-04-06 16:13:29 +0900

    metaの変更

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,7 @@ class Document extends NextDocument<Props> {
       <Html lang="ja">
         <Head>
           <meta charSet="utf-8" />
-          <title>cheerful - エステサロン -</title>
+          <title>ちあふる - エステサロン -</title>
           <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
@@ -44,7 +44,7 @@ class Document extends NextDocument<Props> {
           <meta name="viewport" content="width=device-width,initial-scale=1" />
           <meta
             name="keywords"
-            content="cheerful,ちあふる,エステサロン,奈良県,橿原市,今井町,奈良県橿原市今井町,よもぎ,蓬,水光注射"
+            content="cheerful,ちあふる,エステサロン,奈良県,橿原市,今井町,奈良県橿原市今井町,よもぎ,蓬,水光エステ"
           ></meta>
         </Head>
         <body>
