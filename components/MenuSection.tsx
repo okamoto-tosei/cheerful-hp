f654174b3fe2f33c04f89bcc3122f25dfafc4531
okamoto-tosei <tosei393939@gmail.com>
2023-04-08 01:08:06 +0900

    値段表示の処理修正

--- components/MenuSection.tsx
+++ components/MenuSection.tsx
@@ -15,18 +15,24 @@ export const MenuSection: NextPage<Props> = ({ menus }) => {
       </div>
 
       <div className="box-container">
-        {menus.map((menu) => (
-          <div key={menu.id} className="box">
-            <img
-              className="mask"
-              src={menu.is_img ? `/menu/${menu.img_key}menu.webp` : '/noImage/noimg01.gif'}
-              alt={menu.name}
-            />
-            <h3>{menu.name}</h3>
-            <p>price: ¥{menu.price}(税込)</p>
-            <p>time: {menu.time}分</p>
-          </div>
-        ))}
+        {menus.map((menu) => {
+          const currency = 'JPY'
+          const formatter = new Intl.NumberFormat('ja-JP', { style: 'currency', currency })
+          const amount = formatter.format(menu.price)
+
+          return (
+            <div key={menu.id} className="box">
+              <img
+                className="mask"
+                src={menu.is_img ? `/menu/${menu.img_key}menu.webp` : '/noImage/noimg01.gif'}
+                alt={menu.name}
+              />
+              <h3>{menu.name}</h3>
+              <p>price: {amount}(税込)</p>
+              <p>time: {menu.time}分</p>
+            </div>
+          )
+        })}
       </div>
     </section>
   )
